9023969569c2ddf93046203e1e86fcd54393ef3c
Emmaculate Nzove <emmaculatenzove@EmmmaculateNzove.local>
2022-10-02 10:13:13 +0300

    test project

--- testproject.py
+++ testproject.py
@@ -2,3 +2,7 @@ name = "Jesse"
 print(name)
 print('There are {0} in' " January".format(31))
 print("January : {0}, February: {1}, March: {0}".format(31,28))
+name = "Jesse"
+print(name)
+print('There are {0} in' " January".format(31))
+print("January : {0}, February: {1}, March: {0}".format(31,28))
